f7f6d648caa9e10ad53b1c4536df9291313c8193
kemalztrk06 <58703169+kemalztrk06@users.noreply.github.com>
2020-05-22 22:13:40 +0300

    Add files via upload

---
+++ Kitap.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitapEvi
+{
+    public class Kitap : Program
+    {
+        public Kitap()
+        {
+
+        }
+        public Kitap(string kitapadi, string kitaptürü, string yazaradi, string basimtarihi)
+        {
+            this.KtpAdi = kitapadi;
+
+            this.KtpTürü = kitaptürü;
+
+            this.YzrAdi = yazaradi;
+
+            this.BsmTarihi = basimtarihi;
+        }
+        private string kitapadi;
+
+        private string kitaptürü;
+
+        private string yazaradi;
+
+        private string basimtarihi;
+
+
+        public string KtpAdi { get => kitapadi; set => kitapadi = value; }
+
+        public string KtpTürü { get => kitaptürü; set => kitaptürü = value; }
+
+        public string YzrAdi { get => yazaradi; set => yazaradi = value; }
+
+        public string BsmTarihi { get => basimtarihi; set => basimtarihi = value; }
+
+        public string KtpGetir() => $" Kitap adı:{this.KtpAdi}, Kitap türü: {this.KtpTürü}, Yazar Adı: {this.YzrAdi} , Basım tarihi: {this.BsmTarihi}";
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitapEvi
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Kitap[] kitapevi = new Kitap[0];
+            string cikis = "";
+            while (cikis != "E")
+            {
+                Console.WriteLine("Kütüphanemize Hoşgeldiniz!");
+                Console.WriteLine("Kitap ekleme işlemini yapmak için /1/ tuşuna basınız.");
+                Console.WriteLine("Kitap Listesini görüntüleyebilmek için /2/ tuşuna basınız.");
+                int liste = int.Parse(Console.ReadLine());
+                if (liste == 1)
+                {
+                    Console.WriteLine("Kaç adet kitap girmek istediğinizi yazınız.");
+                    int KitapSecim = int.Parse(Console.ReadLine());
+                    kitapevi = new Kitap[KitapSecim];
+                    for (int i = 0; i < KitapSecim; i++)
+                    {
+                        Kitap kitap1 = new Kitap();
+                        Console.WriteLine("Lütfen {0}. kitap bilgilerini girin.", i + 1);
+                        Console.Write($"Kitabın Adı:");
+                        kitap1.KtpAdi = Console.ReadLine();
+                        Console.Write($"Kitabın Türü:");
+                        kitap1.KtpTürü = Console.ReadLine();
+                        Console.Write($"Kitabın yazarının adı:");
+                        kitap1.YzrAdi = Console.ReadLine();
+                        Console.Write($"Kitabın basım yılı:");
+                        kitap1.BsmTarihi = Console.ReadLine();
+                        Console.WriteLine("_________________________________________________");
+                        kitapevi[i] = kitap1;
+                    }
+
+                }
+                else if (liste == 2)
+                {
+                    Kitap kitap = new Kitap();
+
+                    Console.WriteLine(kitap.KtpGetir());
+                }
+                else
+                {
+                    Console.WriteLine("\n Hatalı girdiniz! Sadece 1 veya 2 kabul edilmektedir.");
+                }
+            }
+
+        }
+    }
+}
