306e99481c40b835f48703feee995be0cdea733c
kemalztrk06 <58703169+kemalztrk06@users.noreply.github.com>
2020-05-22 15:24:58 +0300

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace proje
+{
+    public class Ev
+    {
+        public int odasayisi;
+        public int katno;
+        public string semt;
+        private double alan;
+
+    }
+}
